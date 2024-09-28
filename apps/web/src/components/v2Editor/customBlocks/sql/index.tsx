ebee51680c1047511c92d0640092504e991b863d
Lucas Vieira <vieiralucas4@gmail.com>
2024-09-28 19:01:45 -0300

    fix: missing code editor merge view

--- apps/web/src/components/v2Editor/customBlocks/sql/index.tsx
+++ apps/web/src/components/v2Editor/customBlocks/sql/index.tsx
@@ -232,10 +232,12 @@ function SQLBlock(props: Props) {
   }, [props.block])
 
   const onAcceptAISuggestion = useCallback(() => {
-    // TODO:
-    // acceptDiffEditor()
+    if (aiSuggestions) {
+      updateYText(source, aiSuggestions.toString())
+    }
+
     props.block.setAttribute('aiSuggestions', null)
-  }, [props.block])
+  }, [props.block, aiSuggestions, source])
 
   const onRejectAISuggestion = useCallback(() => {
     props.block.setAttribute('aiSuggestions', null)
@@ -491,24 +493,7 @@ function SQLBlock(props: Props) {
               isCodeHidden ? 'invisible h-0 overflow-hidden' : 'py-5'
             )}
           >
-            <div
-              className={clsx(
-                aiSuggestions === null && 'invisible h-0 overflow-hidden'
-              )}
-            >
-              {/* TODO: */}
-              {/* <DiffEditor */}
-              {/*   key={editorKey} */}
-              {/*   language="sql" */}
-              {/*   onMount={onMountDiffEditor} */}
-              {/*   options={diffEditorOptions} */}
-              {/* /> */}
-            </div>
-            <div
-              className={clsx(
-                aiSuggestions !== null && 'invisible h-0 overflow-hidden'
-              )}
-            >
+            <div>
               <CodeEditor
                 language="sql"
                 source={source}
